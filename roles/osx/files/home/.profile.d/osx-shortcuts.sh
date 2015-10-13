1b63b5345b6357ce8002e52d2745c57ace009881
Justin Buchanan <justbuchanan@gmail.com>
2015-10-13 19:34:16 -0400

    imported osx configs

---
+++ roles/osx/files/home/.profile.d/osx-shortcuts.sh
@@ -0,0 +1,8 @@
+
+alias ki='killall iTunes'
+alias mac_model='sysctl hw.model'
+
+# volume-level (osx only)
+alias stfu="osascript -e 'set volume output muted true'"
+alias pumpitup="osascript -e 'set volume 10'"
+alias mid="osascript -e 'set volume output volume 45'"
