98e2903b22e9c03b34bd2e4c7669431bd41ca19c
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 16:55:37 -0400

    install perl-anyevent for i3

--- arch/i3/install.py
+++ arch/i3/install.py
@@ -7,6 +7,7 @@ yaourt('i3blocks-gaps-git')
 yaourt('i3lock-blur')
 pacman('xautolock')
 pip3('psutil') # needed for some system stats bar items
+pacman('perl-anyevent-i3') # required dependency for savling/loading layouts
 
 base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "home")
 symlink_home(base, '.config/i3blocks')
