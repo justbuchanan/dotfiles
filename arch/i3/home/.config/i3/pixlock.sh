673229bb2313eca5d9f0408498bee7238cf8c73d
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 15:02:59 -0400

    pixellated lock screen for i3

---
+++ arch/i3/home/.config/i3/pixlock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script takes a screenshot and pixellates it, then uses it as the i3lock
+# background.  It was borrowed from here:
+# https://faq.i3wm.org/question/83/how-to-run-i3lock-after-computer-inactivity.1.html
+
+set -e
+
+IMG_PATH=/tmp/i3-pixlock.png
+
+scrot $IMG_PATH
+
+# Pixellate it 10x
+mogrify -scale 10% -scale 1000% $IMG_PATH
+
+# Lock screen displaying this image.
+i3lock -i $IMG_PATH
