14b5a09edb6c0a02cd3204e211886d7bdbb98373
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 15:07:05 -0400

    xautolock

--- arch/i3/install.py
+++ arch/i3/install.py
@@ -5,6 +5,7 @@ from install.utils import *
 yaourt('i3-gaps-git')
 yaourt('i3blocks-gaps-git')
 yaourt('i3lock-blur')
+pacman('xautolock')
 
 base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "home")
 symlink_home(base, '.config/i3blocks')
