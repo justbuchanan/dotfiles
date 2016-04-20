ae79ee5076760a2461a60a73385491078a411cb3
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 10:40:34 -0400

    os-specific install

--- install.py
+++ install.py
@@ -10,5 +10,11 @@ def pacman(pkgname):
 	print("Installing: %s" % pkgname)
 	proc.check_call('pacman -S %s' % pkgname)
 
-import arch.install
+
+import platform
+if platform.release().endswith('ARCH'):
+	print("Installing dotfiles for Arch Linux")
+	import arch.install
+
+print("Installing common dotfiles")
 import common.install
