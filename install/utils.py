756d7a2c8d8e60fdd3993b5bf2da90f39b0e858a
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 20:49:08 -0400

    improved symlink function

--- install/utils.py
+++ install/utils.py
@@ -1,21 +1,19 @@
 import subprocess as proc
 import os
+import sys
 import errno
 
 def symlink_home(srcdir, path):
-    # print("symlinking: %s" % path)
     src = os.path.join(srcdir, path)
     dst = os.path.join(os.path.expanduser('~'), path)
     if os.path.exists(src):
         try:
-            if os.path.islink(dst) and os.readlink(dst) == src:
-                # print('Already linked')
-                pass
-            else:
+            if not os.path.islink(dst) or os.readlink(dst) != src:
+                print("symlinking: %s" % path)
                 mkdir_p(os.path.dirname(dst))
                 proc.check_call(['ln', '-sf', src, dst])
         except OSError as e:
-            print("Unable to link '%s'\n    %s" % (path, str(e)))
+            print("Unable to link '%s'\n    %s" % (path, str(e)), file=sys.stderr)
     else:
         print("Source file doesn't exist")
 
