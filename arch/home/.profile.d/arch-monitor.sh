bb90e080a29869890f7604b4431e2c36d0a6ece0
Justin Buchanan <justbuchanan@gmail.com>
2016-09-13 15:59:50 -0700

    fixed monitor script naming

--- arch/home/.profile.d/arch-monitor.sh
+++ arch/home/.profile.d/arch-monitor.sh
@@ -1,5 +1,4 @@
 
-alias "extmon-left"="xrandr --output DP1 --left-of eDP1 --auto"
-alias "extmon-right"="xrandr --output DP1 --right-of eDP1 --auto"
-alias "extmon-off"="xrandr --output DP1 --off"
-
+alias "extmon-left"="xrandr --output DP-1 --left-of eDP-1 --auto"
+alias "extmon-right"="xrandr --output DP-1 --right-of eDP-1 --auto"
+alias "extmon-off"="xrandr --output DP-1 --off"
