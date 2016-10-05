ef2a2f2e2589d19ec3a64f3044ddcf48e1c49425
Justin Buchanan <justbuchanan@gmail.com>
2016-10-04 21:15:07 -0700

    adapta gtk3 theme

--- arch/theme/install.py
+++ arch/theme/install.py
@@ -1,7 +1,8 @@
 import os
 from install.utils import *
 
-syspkg({'arch': ['vertex-icons-git', 'gtk-theme-arc-git', ]})
+syspkg({'arch': ['vertex-icons-git', 'gtk-theme-arc-git', 'adapta-gtk-theme']})
 
 base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "home")
 symlink_home(base, '.gtkrc-2.0')
+symlink_home(base, '.config/gtk-3.0/settings.ini')
