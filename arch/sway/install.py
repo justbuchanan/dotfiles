fbc5e0d598f4719b69e3bf493312b2cc1f808f9f
Justin Buchanan <justbuchanan@gmail.com>
2016-10-04 23:11:33 -0700

    re-organized sway

--- arch/sway/install.py
+++ arch/sway/install.py
@@ -3,5 +3,4 @@ from install.utils import *
 
 syspkg({'arch': ['sway-git']})
 
-base = os.path.join(os.path.dirname(os.path.realpath(__file__)), "home")
-symlink_home(base, '.config/sway')
+symlink(__file__, 'config', '~/.config/sway')
