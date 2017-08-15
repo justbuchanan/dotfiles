78dd0544487dedf5b9ead3bf9c823c38a9ce418f
Justin Buchanan <justbuchanan@gmail.com>
2017-08-14 20:14:25 -0700

    only install yubikey-manager-qt on graphical systems

--- arch/yubikey/install.py
+++ arch/yubikey/install.py
@@ -3,5 +3,7 @@ from install.utils import *
 syspkg({'arch': [
     'yubikey-personalization-gui-git',
     'libu2f-host',
-    'yubikey-manager-qt',
 ]})
+
+if linux_is_graphical():
+    syspkg({'arch': ['yubikey-manager-qt']})
