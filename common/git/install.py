5b5c02ea1a14dcc8cd61f90b7fd43cab5b452735
Justin Buchanan <justbuchanan@gmail.com>
2018-10-20 00:43:11 -0700

    git diff pdf

--- common/git/install.py
+++ common/git/install.py
@@ -3,3 +3,6 @@ from install.utils import *
 syspkg({'arch': ['git']})
 
 symlink(__file__, 'gitconfig', '~/.gitconfig')
+
+# install 'poppler' for pdfinfo support
+symlink(__file__, 'attributes', '~/.config/git/attributes')
