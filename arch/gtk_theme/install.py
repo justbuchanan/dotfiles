a8c27fc357d2c579bc8de001752de958329f1c08
Justin Buchanan <justbuchanan@gmail.com>
2022-01-18 23:45:59 -0800

    remove obsolete/non-existent package install

--- arch/gtk_theme/install.py
+++ arch/gtk_theme/install.py
@@ -1,7 +1,7 @@
 import os
 from install.utils import *
 
-syspkg({'arch': ['vertex-icons-git', 'gtk-theme-arc-git', 'adapta-gtk-theme']})
+syspkg({'arch': ['vertex-icons-git', 'adapta-gtk-theme']})
 
 symlink(__file__, 'gtkrc-2.0', '~/.gtkrc-2.0')
 symlink(__file__, 'gtk3-settings.ini', '~/.config/gtk-3.0/settings.ini')
