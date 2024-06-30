4594cab805061c067f561a97098a3212e89fa954
Justin Buchanan <justbuchanan@gmail.com>
2024-06-29 23:19:45 -0700

    add nixos configs

---
+++ nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake setup for nixtop";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nixbook = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
