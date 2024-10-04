e47477f6cea948c8851df76dd0e9aca19cda39a2
Justin Buchanan <justbuchanan@gmail.com>
2024-10-03 20:00:53 -0700

    sway: shortcut for switching to new workspace

---
+++ home/.config/sway/scripts/new_workspace.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+# find the number of the rightmost workspace, then switch to a new one right of it
+old_num=$(swaymsg --raw -t get_workspaces | jq '.[].num' | sort -n | tail -1)
+next_num=$(($old_num+1))
+swaymsg workspace $next_num
