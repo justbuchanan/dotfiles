0ae83c9b837da2437507757a1c8499dffce49a1d
Justin Buchanan <justbuchanan@gmail.com>
2024-10-06 21:35:18 -0700

    sway: launch terminal in same working dir as focused window

---
+++ home/.config/sway/scripts/cwd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# prints the current working directory (cwd) for the currently-focused window
+
+# borrowed from https://www.reddit.com/r/swaywm/comments/ayedi1/opening_terminals_at_the_same_directory/
+pid=$(swaymsg --raw -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
+ppid=$(pgrep --newest --parent $pid)
+readlink /proc/${ppid}/cwd || echo $HOME
