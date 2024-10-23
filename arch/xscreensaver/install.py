60092c2a48aa793782e35da9b6e52529491ca836
Justin Buchanan <justbuchanan@gmail.com>
2024-10-22 22:17:35 -0700

    delete old xscreensaver configs

--- arch/xscreensaver/install.py
+++
@@ -1,7 +0,0 @@
-from install.utils import *
-
-syspkg({'arch': ['xscreensaver']})
-
-symlink(__file__, 'pixlock.sh', '~/bin/pixlock')
-symlink(__file__, '.xscreensaver', '~/.xscreensaver')
-symlink(__file__, 'xresources', '~/.Xresources.d/xscreensaver')
