ec2f5b0961606fde44dd5b183da67a63799eb9a2
Justin Buchanan <justbuchanan@gmail.com>
2024-10-24 17:29:12 -0700

    tuigreet: remember last user

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,7 +83,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd sway";
         user = "greeter";
       };
     };
