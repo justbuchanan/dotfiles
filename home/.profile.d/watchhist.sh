fb0d82fe6ff0da2e4323ba386089cccbd9371e33
Justin Buchanan <justbuchanan@gmail.com>
2024-10-25 22:39:42 -0700

    cleanup

--- home/.profile.d/watchhist.sh
+++ home/.profile.d/watchhist.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
-#
-# Print out commands as they are run by watching the history file. Useful for
-# demos.
 
 watchhist() {
     tail -n 1 -f ~/.zsh_history | sed -E 's/.*;(.*)/\1/'
