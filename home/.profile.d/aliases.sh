64c689806200bd22eed1f47d9bdc59ca20f404f4
Justin Buchanan <justbuchanan@gmail.com>
2024-10-28 18:17:47 -0700

    update some bash aliases

--- home/.profile.d/aliases.sh
+++ home/.profile.d/aliases.sh
@@ -1,9 +1,3 @@
-# rails
-alias b='bundle exec'
-
-# show all objc method prototypes in all files in the current directory
-alias show_methods="sed -n '/^[-+]/{s/^.[[:blank:]]*(\([^)]*\))?[[:blank:]]*//s/[[:blank:]]*[;{][[:blank:]]*$//s/:[^:]*([[:blank:]]|$)/:/gp}' *.m"
-
 # open sublime projects
 alias subp='subl --project *.sublime-project'
 
@@ -18,15 +12,11 @@ alias cS='cd ~/src/'
 
 alias s='stylize -i -g master'
 
-alias c='xclip -selection clipboard'
-alias v='xclip -selection clipboard -o'
-
-
+alias c='wl-copy'
+alias v='wl-paste'
 
 alias netscan='nmap  -sn  192.168.0.0/24 | grep -v "Host is up" | sed "s/Nmap scan report for //g"'
-
 
 alias open="xdg-open $@"
 
 alias say="echo '$*' | espeak"
-
