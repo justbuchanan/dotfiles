189b5c0f1efb4216bb2ac5f63094114dd52e3c81
Justin Buchanan <justbuchanan@gmail.com>
2025-10-15 16:51:01 -0700

    change tuigreet default to niri

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -138,7 +138,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
         user = "greeter";
       };
     };
