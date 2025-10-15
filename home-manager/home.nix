203af0d5dabeb5701b37209b5b032c1e66ecf0e1
Justin Buchanan <justbuchanan@gmail.com>
2025-10-14 23:25:48 -0700

    use home manager to manage more files

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,32 @@
     ./hyprland.nix
   ];
 
+  home.file = {
+    ".gitconfig".source = ../home/.gitconfig;
+    ".config/git/ignore".source = ../home/.config/git/ignore;
+    ".config/git/attributes".source = ../home/.config/git/attributes;
+
+    ".tigrc".source = ../home/.tigrc;
+    ".vimrc".source = ../home/.vimrc;
+    ".vim".source = ../home/.vim;
+    ".bashrc".source = ../home/.bashrc;
+    ".zshrc".source = ../home/.zshrc;
+    ".p10k.zsh".source = ../home/.p10k.zsh;
+    ".tmux.conf".source = ../home/.tmux.conf;
+
+    ".config/foot/foot.ini".source = ../home/.config/foot/foot.ini;
+
+    ".profile.d".source = ../home/.profile.d;
+
+    ".config/nixpkgs/config.nix".source = ../home/.config/nixpkgs/config.nix;
+
+    ".config/sway".source = ../home/.config/sway;
+    ".config/sworkstyle/config.toml".source = ../home/.config/sworkstyle/config.toml;
+
+    ".config/gtk-3.0/settings.ini".source = ../home/.config/gtk-3.0/settings.ini;
+    ".gtkrc-2.0".source = ../home/.gtkrc-2.0;
+  };
+
   home.sessionVariables = {
     EDITOR = "vim";
   };
