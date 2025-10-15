9836fd36d475f020ffc8511801f7789986914033
Justin Buchanan <justbuchanan@gmail.com>
2025-10-14 19:30:28 -0700

    delete some old stuff

--- arch/cli_visualizer/install.py
+++
@@ -1,5 +0,0 @@
-from install.utils import *
-
-syspkg({'arch': ['cli-visualizer-git']})
-
-symlink(__file__, 'vis', '~/.config/vis')
--- arch/other/.profile.d/arch-monitor.sh
+++
@@ -1,4 +0,0 @@
-
-alias "extmon-left"="xrandr --output DP-1 --left-of eDP-1 --auto"
-alias "extmon-right"="xrandr --output DP-1 --right-of eDP-1 --auto"
-alias "extmon-off"="xrandr --output DP-1 --off"
