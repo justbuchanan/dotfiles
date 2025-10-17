1c8c0823a8c9fd579399ab73233a7a0f62c7e3a2
Justin Buchanan <justbuchanan@gmail.com>
2025-10-16 20:09:55 -0700

    adjust niri cwd.sh to get the right dir for sublime windows

--- home/.config/niri/scripts/cwd.sh
+++ home/.config/niri/scripts/cwd.sh
@@ -3,7 +3,9 @@
 # prints the current working directory (cwd) for the currently-focused window
 
 # Get the focused window's PID from niri
-pid=$(niri msg -j focused-window | jq -r '.pid')
+focused_window="$(niri msg -j focused-window)"
+pid=$(echo "$focused_window" | jq -r '.pid')
+app_id=$(echo "$focused_window" | jq -r '.app_id')
 
 # if no pid found, use home dir
 if [[ -z $pid ]] || [[ $pid == "null" ]]; then
@@ -11,6 +13,12 @@ if [[ -z $pid ]] || [[ $pid == "null" ]]; then
     exit 0
 fi
 
-ppid=$(pgrep --newest --parent $pid)
+if [[ "$app_id" == "sublime_text" ]]; then
+    # for sublime, use the pid directly
+    ppid=$pid
+else
+    # for terminals, and others, use the parent process id
+    ppid=$(pgrep --newest --parent $pid)
+fi
 # if no cwd found, use home dir
 readlink /proc/${ppid}/cwd || echo $HOME
