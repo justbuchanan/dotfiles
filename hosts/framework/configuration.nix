4d1fe0d2e7665c3288d61841aab5da97c4562a5f
Justin Buchanan <justbuchanan@gmail.com>
2025-10-17 21:01:01 -0700

    nix: enable weekly gc

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -12,6 +12,11 @@
     "nix-command"
     "flakes"
   ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
   nixpkgs.config.allowUnfree = true;
 
   imports = [
