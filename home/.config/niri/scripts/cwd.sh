606fd14360535d1b602f8d311ca9cc9ece1e8fb8
Justin Buchanan <justbuchanan@gmail.com>
2025-10-23 21:57:33 -0700

    niri cwd.sh: detect if no window is focused

--- home/.config/niri/scripts/cwd.sh
+++ home/.config/niri/scripts/cwd.sh
@@ -2,14 +2,23 @@
 
 # prints the current working directory (cwd) for the currently-focused window
 
+DEFAULT_DIR="$HOME"
+
 # Get the focused window's PID from niri
 focused_window="$(niri msg -j focused-window)"
+
+# If nothing's focused, use default
+if [[ "$focused_window" == "null" ]]; then
+    echo "$DEFAULT_DIR"
+    exit 0
+fi
+
 pid=$(echo "$focused_window" | jq -r '.pid')
 app_id=$(echo "$focused_window" | jq -r '.app_id')
 
-# if no pid found, use home dir
+# if no pid found, use default
 if [[ -z $pid ]] || [[ $pid == "null" ]]; then
-    echo $HOME
+    echo "$DEFAULT_DIR"
     exit 0
 fi
 
