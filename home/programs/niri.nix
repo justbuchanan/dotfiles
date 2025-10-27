5cd8c70142a87ff9a7d7b59f5b5a8b6cd7ec678d
Justin Buchanan <justbuchanan@gmail.com>
2025-10-27 10:27:31 -0700

    add cache; change wallpaper

--- home/programs/niri.nix
+++ home/programs/niri.nix
@@ -81,7 +81,7 @@ in
           argv = [
             "swaybg"
             "--image"
-            "/home/justin/src/justin/dotfiles/wallpapers/artist_point.jpg"
+            "/home/justin/src/justin/dotfiles/wallpapers/maple_loop_pass.jpg"
           ];
         }
         {
---
+++ nixos/cachix/justbuchanan-cq-flake.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://justbuchanan-cq-flake.cachix.org"
+      ];
+      trusted-public-keys = [
+        "justbuchanan-cq-flake.cachix.org-1:xR50NcQDylGOIydzbXzW4dIwsoFEnbVb0GT0XU1M7mc="
+      ];
+    };
+  };
+}
