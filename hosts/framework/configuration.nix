cf3c6fe780865bd06312418029bce21abe62c1b1
Justin Buchanan <justbuchanan@gmail.com>
2025-10-29 21:32:41 -0700

    laptop: mount darktable library over samba

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -156,6 +156,16 @@
       in
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
+  fileSystems."/mnt/srvbox_darktable_justin" = {
+    device = "//srvbox.wampus-newton.ts.net/justin-darktable";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+  };
   services.gvfs.enable = true;
 
   # greetd tui login manager
