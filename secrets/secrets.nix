2491d65adbd5dd92e6d177c4f5214b1971d37577
Justin Buchanan <justbuchanan@gmail.com>
2025-10-29 22:13:04 -0700

    add key to secrets. need to rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,18 +6,22 @@ let
   # pub keys
   droplet1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMrFAYD7bbkHKnnJ8DsDYpropw9N/OeIPl+8h27/ed1 root@droplet1";
   justin-srvbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAV1Opx5znTrZ2vsFCifxScZ7+2H3Z63KebGBoXGmvRs justin@srvbox";
+  justin-framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4tJlxrvlz/mhLa6AZ8N0y8cezN1vQrkuA8Dpv7kr+6 justin@framework";
 in
 {
   "droplet1-authelia-jwt-secret.age".publicKeys = [
     droplet1
     justin-srvbox
+    justin-framework
   ];
   "droplet1-authelia-encryption-key.age".publicKeys = [
     droplet1
     justin-srvbox
+    justin-framework
   ];
   "droplet1-authelia-users.age".publicKeys = [
     droplet1
     justin-srvbox
+    justin-framework
   ];
 }
