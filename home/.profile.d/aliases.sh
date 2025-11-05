853b2c825110ad168c6877d0e8169761c034fcc8
Justin Buchanan <justbuchanan@gmail.com>
2025-11-04 22:19:51 -0800

    setup samba

--- home/.profile.d/aliases.sh
+++ home/.profile.d/aliases.sh
@@ -29,7 +29,8 @@ say() {
     echo "$@" | espeak
 }
 
-alias nrs='nixos-rebuild switch --sudo'
+# alias nrs='nixos-rebuild switch --sudo'
+alias nrs='sudo nixos-rebuild switch'
 
 export HM_DIR=/home/justin/src/justin/dotfiles
 alias hms='home-manager switch --flake $HM_DIR'
--- hosts/srvbox/configuration.nix
+++ hosts/srvbox/configuration.nix
@@ -98,6 +98,8 @@ in
 
   networking.hostName = "srvbox";
   networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
 
   networking.nameservers = [
     "1.1.1.1"
@@ -125,6 +127,58 @@ in
     pkgs.cups-dymo
   ];
   services.system-config-printer.enable = true;
+
+  # samba file sharing
+  # beyond this config, a password for each user must be set with:
+  # smbpasswd -a <user>
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "server string" = "smbnix";
+        "server role" = "standalone server";
+        "disable netbios" = "yes";
+        "smb ports" = "445";
+        "logging" = "systemd";
+
+        # server-side copy for macOS users - https://wiki.archlinux.org/title/Samba
+        "fruit:copyfile" = "yes";
+
+        # performance tuning
+        # https://www.reddit.com/r/OpenMediaVault/comments/11gwi1g/significant_samba_speedperformance_improvement_by/
+        "min receivefile size" = "16384";
+        "use sendfile" = "yes";
+        "vfs objects" = "io_uring";
+        "aio read size" = "16384";
+        "aio write size" = "16384";
+      };
+      justin = {
+        "path" = "/mnt/zpool0/samba/justin";
+        "browsable" = "true";
+        "read only" = "false";
+        "force create mode" = "0660";
+        "force directory mode" = "2770";
+        "valid users" = "justin";
+      };
+      justin-darktable = {
+        "path" = "/mnt/zpool0/photos/darktable";
+        "browsable" = "true";
+        "read only" = "false";
+        "force create mode" = "0660";
+        "force directory mode" = "2770";
+        "valid users" = "justin";
+      };
+      avani = {
+        "path" = "/mnt/zpool0/samba/avani";
+        "browsable" = "true";
+        "read only" = "false";
+        "force create mode" = "0660";
+        "force directory mode" = "2770";
+        "valid users" = "avani";
+      };
+    };
+  };
 
   # # enable bluetooth
   # hardware.bluetooth.enable = true;
@@ -219,6 +273,9 @@ in
       "video" # allow changing screen brightness (among other things) without sudo
       "dialout" # access to /dev/tty* devices without sudo
     ];
+  };
+  users.users.avani = {
+    isNormalUser = true;
   };
 
   # font awesome is needed for waybar to work correctly
