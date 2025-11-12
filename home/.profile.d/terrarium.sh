0e88cb9c2b5690ea02de8489a8b168e3608e40c6
Justin Buchanan <justbuchanan@gmail.com>
2025-11-12 12:37:37 -0800

    update terrarium config

--- home/.profile.d/terrarium.sh
+++ home/.profile.d/terrarium.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export TERRARIUM_ADDR=glassbox
-alias tcli='$HOME/src/justin/terrarium/code/client.py'
+export TERRARIUM_ADDR=192.168.68.72
 
 export OASIS_ADDR=oasis-justin.local
