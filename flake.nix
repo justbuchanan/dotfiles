justbuchanan/dotfiles

commit 7520338755af0f8c9c42d5799ee73024aee4f42b

some niri updates; install via home-manager, not system module

hosts/shared/graphical-and-personal.nix
@@ -91,9 +91,6 @@
     TTYVTDisallocate = true;
   };
 
-  # window manager
-  programs.niri.enable = true;
-
   # Enable sound.
   services.pipewire = {
     enable = true;

home/programs/niri.nix
@@ -19,6 +19,7 @@ in
   programs.niri.package = pkgs.niri-unstable;
 
   programs.niri = {
+    enable = true;
     settings = {
       hotkey-overlay = {
         skip-at-startup = true;

hosts/framework/configuration.nix
@@ -11,7 +11,6 @@
     ./hardware-configuration.nix
     # binary cache server
     ../../nixos/cachix.nix
-    inputs.niri.nixosModules.niri
     ../shared/base.nix
     ../shared/graphical-and-personal.nix
   ];

hosts/srvbox/configuration.nix
@@ -20,7 +20,6 @@
     ../shared/graphical-and-personal.nix
     # binary cache server
     ../../nixos/cachix.nix
-    inputs.niri.nixosModules.niri
   ];
 
   boot = {

commit ca01e3d96ab1bc31ce427d29c662828693665596

partially revert 7520338755af0f8c9c42d5799ee73024aee4f42b
caused issues with swaylock refusing to unlock

flake.nix
@@ -80,6 +80,7 @@
             agenix.nixosModules.default
             # https://github.com/NixOS/nixos-hardware/tree/master/framework/13-inch/13th-gen-intel
             nixos-hardware.nixosModules.framework-13th-gen-intel
+            inputs.niri.nixosModules.niri
           ];
         };
 
@@ -93,6 +94,7 @@
             ./hosts/srvbox/configuration.nix
             determinate.nixosModules.default
             agenix.nixosModules.default
+            inputs.niri.nixosModules.niri
           ];
         };
 

hosts/shared/graphical-and-personal.nix
@@ -91,6 +91,9 @@
     TTYVTDisallocate = true;
   };
 
+  # window manager
+  programs.niri.enable = true;
+
   # Enable sound.
   services.pipewire = {
     enable = true;