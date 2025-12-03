d95541ed938e194c223fd29b21087189d6cb472b
Justin Buchanan <justbuchanan@gmail.com>
2025-12-02 18:39:15 -0800

    lock down prometheus exporter port to tailscale-only

--- hosts/shared/prometheus-exporter.nix
+++ hosts/shared/prometheus-exporter.nix
@@ -6,5 +6,7 @@
     port = 9100;
     enabledCollectors = [ "systemd" ];
   };
-  networking.firewall.allowedTCPPorts = [ 9100 ];
+
+  # Only allow access from Tailscale
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 9100 ];
 }
